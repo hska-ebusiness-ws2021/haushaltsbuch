438cbcd4c05adb30edaa5b26d3231934e04bc132
Ari1012 <ariadna0319@gmail.com>
2021-01-04 21:23:17 +0100

    Fake login

---
+++ app/src/main/java/com/example/haushaltsbuch/ui/login/LoginDataSource.kt
@@ -0,0 +1,5 @@
+package com.example.haushaltsbuch.ui.login
+
+class LoginDataSource {
+
+}
--- app/src/main/java/com/example/haushaltsbuch/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/example/haushaltsbuch/ui/login/LoginViewModel.kt
@@ -19,8 +19,8 @@ class LoginViewModel(private val loginRepository: LoginRepository) : ViewModel()
         // can be launched in a separate asynchronous job
         val result = loginRepository.login(username, password)
 
-        if (result is Result.Success) {
-            _loginResult.value = LoginResult(success = LoggedInUserView(displayName = result.data.displayName))
+        if (result) {
+            _loginResult.value = LoginResult(success = LoggedInUserView(displayName = "result.data.displayName"))
         } else {
             _loginResult.value = LoginResult(error = R.string.login_failed)
         }
