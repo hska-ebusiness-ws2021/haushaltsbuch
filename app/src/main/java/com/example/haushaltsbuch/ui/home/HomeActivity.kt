c2cdb0d2b70c2a1b142da82dd2c8d2670ca4aa9f
LarsSchmider <lars.schmider@live.com>
2021-01-11 18:25:51 +0100

    basic menu

---
+++ app/src/main/java/com/example/haushaltsbuch/ui/home/HomeActivity.kt
@@ -0,0 +1,19 @@
+package com.example.haushaltsbuch.ui.home
+
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import com.example.haushaltsbuch.R
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+        setSupportActionBar(findViewById(R.id.toolbar))
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+    }
+}
