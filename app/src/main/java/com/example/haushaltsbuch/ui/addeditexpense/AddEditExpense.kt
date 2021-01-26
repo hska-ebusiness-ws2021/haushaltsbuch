cdbda74ff179f151fbe8498d6a04f992534c4fb6
Florian Goebel <florian.goebel@robodev.eu>
2021-01-26 11:25:23 +0100

    Update add expense

--- app/src/main/java/com/example/haushaltsbuch/ui/addeditexpense/AddEditExpense.kt
+++ app/src/main/java/com/example/haushaltsbuch/ui/addeditexpense/AddEditExpense.kt
@@ -1,6 +1,7 @@
 package com.example.haushaltsbuch.ui.addeditexpense
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.view.View.GONE
 import android.widget.*
@@ -15,6 +16,8 @@ import com.example.haushaltsbuch.data.model.finances.Expense
 import com.example.haushaltsbuch.database.DBHelper
 import com.example.haushaltsbuch.databinding.ContentAddEditExpenseBinding
 import com.example.haushaltsbuch.ui.DatePickerFragment
+import com.example.haushaltsbuch.ui.home.HomeActivity
+import com.example.haushaltsbuch.ui.login.LoginActivity
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.textview.MaterialTextView
 import org.joda.time.DateTime
@@ -24,11 +27,6 @@ import java.util.*
 
 class AddEditExpense : AppCompatActivity() {
     private val model: AddExpenseViewModel by viewModels()
-
-    companion object {
-        const val IS_EINNAHMEN = "isEinnahmen"
-    }
-
 
     val parties = arrayOf(
         "Lebensmittel",
@@ -40,7 +38,6 @@ class AddEditExpense : AppCompatActivity() {
         "Studium",
         "Urlaub"
     )
-    var i = 0.7
 
     private lateinit var binding: ContentAddEditExpenseBinding
 
@@ -50,51 +47,34 @@ class AddEditExpense : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
-        //val saveButton = binding.buttonSave
 
         setContentView(R.layout.content_add_edit_expense)
         setSupportActionBar(findViewById(R.id.toolbar))
+
+        // ===================================================================
+        //                         Link UI elements
+        // ===================================================================
         val amount = findViewById<EditText>(R.id.editTextAmount)
         val category = findViewById<Spinner>(R.id.tags_spinner)
         val imageTag = findViewById<AppCompatImageView>(R.id.imageTags)
-        val expenseAdapter = CategoryAdapter(model.expenseCategorieList)
         val recyclerView: RecyclerView = findViewById(R.id.recycler_view)
-        recyclerView.adapter = expenseAdapter
         val addButton = findViewById<FloatingActionButton>(R.id.button_add)
-        addButton.setOnClickListener {
-            model.addExpenseCategorieEntry(
-                TextWithAmount(
-                    category.selectedItem.toString(),
-                    amount.text.toString().toDouble()
-                )
-            )
-            Toast.makeText(this, "add!", Toast.LENGTH_SHORT).show()
-            expenseAdapter.notifyDataSetChanged()
-        }
+        val cancelButton = findViewById<Button>(R.id.button_cancel)
+        val backButton = findViewById<Button>(R.id.button_back)
         val date = findViewById<MaterialTextView>(R.id.textDate)
         val saveButton = findViewById<Button>(R.id.button_save)
-
-        saveButton.setOnClickListener {
-            val amount = BigDecimal(amount.text.toString())
-            val id = UUID.randomUUID()
-            val category = category.selectedItem.toString()
-            val date = DateTime(model.date.value)
-            val dbHelper = DBHelper()
-            dbHelper.addExpense(Expense(id, amount, Category(category), date))
-            Toast.makeText(this, "Save!", Toast.LENGTH_SHORT).show()
-            setResult(Activity.RESULT_OK);
-        }
-
         val spinner: Spinner = findViewById(R.id.tags_spinner)
+        val title = findViewById<TextView>(R.id.titleExpenses)
+        // ===================================================================
+
+
+
+        val expenseAdapter = CategoryAdapter(model.expenseCategorieList)
+        recyclerView.adapter = expenseAdapter
+
+
         val arrayTags = ArrayAdapter(this, android.R.layout.simple_spinner_item, parties)
         spinner.adapter = arrayTags
-
-        date.setOnClickListener {
-            val newFragment = DatePickerFragment(model.date)
-            newFragment.show(supportFragmentManager, "dataPicker")
-        }
-
-        val title = findViewById<TextView>(R.id.titleExpenses)
 
         val isEinnahme: Boolean = intent.getBooleanExtra(IS_EINNAHMEN, false)
         if (isEinnahme) {
@@ -110,5 +90,50 @@ class AddEditExpense : AppCompatActivity() {
             date.text = format.format(it)
         })
 
+        // ===================================================================
+        //                   Bind Listeners to UI elements
+        // ===================================================================
+
+        addButton.setOnClickListener {
+            model.addExpenseCategorieEntry(
+                TextWithAmount(
+                    category.selectedItem.toString(),
+                    amount.text.toString().toDouble()
+                )
+            )
+            Toast.makeText(this, "add!", Toast.LENGTH_SHORT).show()
+            expenseAdapter.notifyDataSetChanged()
+        }
+
+        saveButton.setOnClickListener {
+            val amount = BigDecimal(amount.text.toString())
+            val id = UUID.randomUUID()
+            val category = category.selectedItem.toString()
+            val date = DateTime(model.date.value)
+            val dbHelper = DBHelper()
+            dbHelper.addExpense(Expense(id, amount, Category(category), date))
+            Toast.makeText(this, "Save!", Toast.LENGTH_SHORT).show()
+            setResult(Activity.RESULT_OK);
+        }
+
+        date.setOnClickListener {
+            val newFragment = DatePickerFragment(model.date)
+            newFragment.show(supportFragmentManager, "dataPicker")
+        }
+
+        cancelButton.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
+
+        backButton.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    companion object {
+        const val IS_EINNAHMEN = "isEinnahmen"
     }
 }
+
